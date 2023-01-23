04f6c01f44cd9477f09434c142256b160f5f138a
= <=>
2023-01-23 14:55:56 +0100

    init

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMesage(string newMessage) public {
+        message = newMessage;
+    }
+
+}
